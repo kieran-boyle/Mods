dca3959fa3c08a04b1d95eedeb79167eee7f9582
kieran-boyle <kieran.boyle1@outlook.com>
2022-02-14 17:22:21 +0000

    BS Linting.

--- Kiki-BiggerStash2.1.5/mod.js
+++ Kiki-BiggerStash2.1.5/mod.js
@@ -1,13 +1,14 @@
-"use strict";
+"use strict"
 
-const config = require("./config/config.json");
-const BiggerStash = require("./src/BiggerStash.js");
+const BiggerStash = require("./src/BiggerStash.js")
 
 class Mod {
-		constructor() {
-			Logger.info("Loading: Kiki-BiggerStash");
-			ModLoader.onLoad["Kiki-BiggerStash"] = BiggerStash.onLoadMod;
-		}	
+
+	constructor() {
+
+		Logger.info("Loading: Kiki-BiggerStash")
+		ModLoader.onLoad["Kiki-BiggerStash"] = BiggerStash.onLoadMod
+	}
 }
 
-module.exports.Mod = new Mod();
+module.exports.Mod = new Mod()
