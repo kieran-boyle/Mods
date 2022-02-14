2b7d512d7e87b75dc3ccd358beda61061600b5bb
kieran-boyle <kieran.boyle1@outlook.com>
2022-02-14 18:29:55 +0000

    RTQC Linting.

--- Kiki-RemoveTediousQuestConditions1.0.2/mod.js
+++ Kiki-RemoveTediousQuestConditions1.0.2/mod.js
@@ -3,10 +3,12 @@
 const RemoveTediousQuestConditions = require("./src/RemoveTediousQuestConditions.js")
 
 class Mod {
-		constructor() {
-			Logger.info("Loading: Kiki-RemoveTediousQuestConditions")
-			ModLoader.onLoad["Kiki-RemoveTediousQuestConditions"] = RemoveTediousQuestConditions.onLoadMod
-		}	
+
+	constructor() {
+		
+		Logger.info("Loading: Kiki-RemoveTediousQuestConditions")
+		ModLoader.onLoad["Kiki-RemoveTediousQuestConditions"] = RemoveTediousQuestConditions.onLoadMod
+	}
 }
 
 module.exports.Mod = new Mod()
--- Kiki-RemoveTediousQuestConditions1.0.2/src/RemoveTediousQuestConditions.js
+++ Kiki-RemoveTediousQuestConditions1.0.2/src/RemoveTediousQuestConditions.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const config = require("../config/config.json")
+const CONFIG = require("../config/config.json")
 
 class RemoveTediousQuestConditions {
 
@@ -23,8 +23,42 @@ class RemoveTediousQuestConditions {
             "without"
         ]
 
-        for (let eachOption in config) {
-            if (config[eachOption] != false) {
+        //Recursive function that clears a given keys value to [] in a nested object.
+        var keyClearer = function (object, key, questId) {
+
+            if (Object.prototype.hasOwnProperty.call(object, key)) {
+                localesCleaner(questId)
+                key !== "distance" ? object[key] = [] : object[key].value = 0
+            }
+
+            for (var i = 0; i < Object.keys(object).length; i++) {
+                if (typeof object[Object.keys(object)[i]] == "object")
+                    keyClearer(object[Object.keys(object)[i]], key, questId)
+            }
+        }
+
+        var localesCleaner = function (questId) {
+
+            for (let condition in locales[questId].conditions) {
+                let thisCondition = locales[questId].conditions[condition].split(" ")
+
+                if (conditionFirstWord.includes(thisCondition[0])) {
+                    let endWord = findEnd(thisCondition)
+                    locales[questId].conditions[condition] = thisCondition.slice(0, endWord).join(" ")
+                }
+            }
+        }
+
+        var findEnd = function (thisCondition) {
+
+            for (let word of thisCondition) {
+                if (conditionTargetWord.includes(word))
+                    return thisCondition.indexOf(word)
+            }
+        }
+
+        for (let eachOption in CONFIG) {
+            if (CONFIG[eachOption] != false) {
                 switch (eachOption) {
                     case "setPMC":
                         quests["5a27c99a86f7747d2c6bdd8e"].conditions.AvailableForFinish[0]._props.counter.conditions[0]._props.target = "AnyPmc"
@@ -41,40 +75,6 @@ class RemoveTediousQuestConditions {
             for (let eachQuest in quests)
                 keyClearer(quests[eachQuest], condition, eachQuest)
         })
-
-        //Recursive function that clears a given keys value to [] in a nested object.
-        function keyClearer(object, key, questId) {
-
-            if (object.hasOwnProperty(key)) {
-                localesCleaner(questId)
-                key !== "distance" ? object[key] = [] : object[key].value = 0
-            }
-
-            for (var i = 0; i < Object.keys(object).length; i++) {
-                if (typeof object[Object.keys(object)[i]] == "object")
-                    keyClearer(object[Object.keys(object)[i]], key, questId)
-            }
-        }
-
-        function localesCleaner(questId) {
-            
-            for (let condition in locales[questId].conditions) {
-                let thisCondition = locales[questId].conditions[condition].split(" ")
-
-                if (conditionFirstWord.includes(thisCondition[0])) {
-                    let endWord = findEnd(thisCondition)
-                    locales[questId].conditions[condition] = thisCondition.slice(0, endWord).join(" ")
-                }
-            }
-        }
-
-        function findEnd(thisCondition) {
-
-            for (let word of thisCondition) {
-                if (conditionTargetWord.includes(word))
-                    return thisCondition.indexOf(word)
-            }
-        }
     }
 }
 
