5b8d5f3465c264e83e53b36897d88fd480992573
kieran-boyle <kieran.boyle1@outlook.com>
2022-02-14 20:27:22 +0000

    Fix for issue #1

--- Kiki-TraderLevelTweeks/src/TraderLevelTweeks.js
+++ Kiki-TraderLevelTweeks/src/TraderLevelTweeks.js
@@ -9,32 +9,19 @@ class TraderLevelTweeks {
 
 		for (let trader in TRADERS) {
 
-			if (CONFIG.cantBuy === true) {
+			if (CONFIG.cantBuy === true)
 				TRADERS[trader].base.sell_category = []
-			}
 
 			for (let eachLevel in TRADERS[trader].base.loyaltyLevels) {
 
-				if (CONFIG.minLevel === true) {
+				if (CONFIG.minLevel === true)
+					TRADERS[trader].base.loyaltyLevels[eachLevel].minLevel = 0
 
-					for (let minLevel in TRADERS[trader].base.loyaltyLevels[eachLevel]) {
-						TRADERS[trader].base.loyaltyLevels[eachLevel].minLevel = 0
-					}
-				}
+				if (CONFIG.minSalesSum === true)
+					TRADERS[trader].base.loyaltyLevels[eachLevel].minSalesSum = 0
 
-				if (CONFIG.minSalesSum === true) {
-
-					for (let minSalesSum in TRADERS[trader].base.loyaltyLevels[eachLevel]) {
-						TRADERS[trader].base.loyaltyLevels[eachLevel].minSalesSum = 0
-					}
-				}
-
-				if (CONFIG.minStanding === true) {
-
-					for (let minStanding in TRADERS[trader].base.loyaltyLevels[eachLevel]) {
-						TRADERS[trader].base.loyaltyLevels[eachLevel].minStanding = 0
-					}
-				}
+				if (CONFIG.minStanding === true)
+					TRADERS[trader].base.loyaltyLevels[eachLevel].minStanding = 0
 			}
 		}
 	}
