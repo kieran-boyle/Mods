bca9e4d60ee418d91fdd02e9ccefb5cd1e2a82b0
kieran-boyle <kieran.boyle1@outlook.com>
2022-02-14 16:13:00 +0000

    ATB Linted

--- Kiki-AllTheBoss2.3.1/src/AllTheBoss.js
+++ Kiki-AllTheBoss2.3.1/src/AllTheBoss.js
@@ -8,58 +8,15 @@ const config = require("../config/config.json")
 var zones = []
 
 class AllTheBoss {
-    static onLoadMod() {
-        for (let eachMap in maps) {
-			let thisMap = []
-            if (config[eachMap].enabled === true) {
-				zones = maps[eachMap].split(",")
-                for (let eachEntry in allBoss) {
-					let thisBoss = allBoss[eachEntry]
-					if(config[eachMap].bossList[thisBoss.BossName] > 0){
-						thisBoss.BossChance = config[eachMap].bossList[thisBoss.BossName]
-						thisBoss.BossZone = chooseZone(zones)
-						if (zones.length <= 1) {
-							zones = maps[eachMap].split(",")
-						}
-						thisMap.push(thisBoss)
-						if (config.debug === true) {
-							Logger.log(`[Kiki-AllTheBoss] : ${thisBoss.BossName} has a ${thisBoss.BossChance}% chance to spawn on ${eachMap}`, "yellow", "black")
-						}
-					}
-                }
-				 if(eachMap === "rezervbase"){
-					for(let eachBot in raiders.rezervbase){
-						let thisRaider = raiders.rezervbase[eachBot]
-						if(config.boostRaiders.enabled === true){
-							thisRaider.BossChance = config.boostRaiders.chance
-							thisRaider.Time = config.boostRaiders.time
-							thisRaider.BossEscortAmount = config.boostRaiders.escortAmount -1
-						}
-						thisMap.push(thisRaider)
-					}
-				} 
-				if(eachMap === "laboratory"){
-					for(let eachBot in raiders.laboratory){
-						let thisRaider = raiders.laboratory[eachBot]
-						if(config.boostRaiders.enabled === true){
-							thisRaider.BossChance = config.boostRaiders.chance
-							thisRaider.Time = config.boostRaiders.time
-							thisRaider.BossEscortAmount = config.boostRaiders.escortAmount - 1
-						}
-						thisMap.push(thisRaider)
-					}
-				}
-				eachMap === "lighthouse" ?
-					database[eachMap].base.BossLocationSpawn =[...database[eachMap].base.BossLocationSpawn, ...thisMap] :
-					database[eachMap].base.BossLocationSpawn = thisMap 
 
-            }
-        }
-		function getRandomInt(max) {
+	static onLoadMod() {
+
+		var getRandomInt = function (max) {
 			return Math.floor(Math.random() * max);
 		}
 
-		function chooseZone(zoneList) {
+		var chooseZone = function (zoneList) {
+
 			if (zoneList.length === 1) {
 				return zoneList.toString()
 			}
@@ -68,6 +25,65 @@ class AllTheBoss {
 			zones.splice(rand, 1)
 			return thisZone.toString()
 		}
-    }
+
+		for (let eachMap in maps) {
+			let thisMap = []
+
+			if (config[eachMap].enabled === true) {
+				zones = maps[eachMap].split(",")
+
+				for (let eachEntry in allBoss) {
+					let thisBoss = allBoss[eachEntry]
+
+					if (config[eachMap].bossList[thisBoss.BossName] > 0) {
+						thisBoss.BossChance = config[eachMap].bossList[thisBoss.BossName]
+						thisBoss.BossZone = chooseZone(zones)
+
+						if (zones.length <= 1) {
+							zones = maps[eachMap].split(",")
+						}
+
+						thisMap.push(thisBoss)
+
+						if (config.debug === true) {
+							Logger.log(`[Kiki-AllTheBoss] : ${thisBoss.BossName} has a ${thisBoss.BossChance}% chance to spawn on ${eachMap}`, "yellow", "black")
+						}
+					}
+				}
+
+				if (eachMap === "rezervbase") {
+
+					for (let eachBot in raiders.rezervbase) {
+						let thisRaider = raiders.rezervbase[eachBot]
+
+						if (config.boostRaiders.enabled === true) {
+							thisRaider.BossChance = config.boostRaiders.chance
+							thisRaider.Time = config.boostRaiders.time
+							thisRaider.BossEscortAmount = config.boostRaiders.escortAmount - 1
+						}
+						thisMap.push(thisRaider)
+					}
+				}
+
+				if (eachMap === "laboratory") {
+
+					for (let eachBot in raiders.laboratory) {
+						let thisRaider = raiders.laboratory[eachBot]
+
+						if (config.boostRaiders.enabled === true) {
+							thisRaider.BossChance = config.boostRaiders.chance
+							thisRaider.Time = config.boostRaiders.time
+							thisRaider.BossEscortAmount = config.boostRaiders.escortAmount - 1
+						}
+						thisMap.push(thisRaider)
+					}
+				}
+
+				eachMap === "lighthouse" ?
+					database[eachMap].base.BossLocationSpawn = [...database[eachMap].base.BossLocationSpawn, ...thisMap] :
+					database[eachMap].base.BossLocationSpawn = thisMap
+			}
+		}
+	}
 }
 module.exports = AllTheBoss
