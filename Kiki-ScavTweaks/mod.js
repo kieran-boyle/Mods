7d796544360137c3e94747a884bc541503c47b7f
kieran-boyle <kieran.boyle1@outlook.com>
2022-03-09 19:13:51 +0000

    Added onLoad

--- Kiki-ScavTweaks/mod.js
+++ Kiki-ScavTweaks/mod.js
@@ -7,6 +7,7 @@ class ScavTweaks
   {
     Logger.info("Loading: Kiki-ScavTweaks")
     HttpRouter.onStaticRoute["/raid/profile/save"]["scav"] = ScavTweaks.setScavCooldown
+    ModLoader.onLoad["Kiki-ScavTweaks"] = ScavTweaks.onLoadMod
   }
 
   static setScavCooldown(url, obj, id, output)
@@ -37,6 +38,12 @@ class ScavTweaks
     }
 
     return output
+  }
+
+  static onLoadMod()
+  {
+    const config = require("./config/config.json")
+    DatabaseServer.tables.globals.config.SavagePlayCooldown = config.ScavTimeDead
   }
 }
 
