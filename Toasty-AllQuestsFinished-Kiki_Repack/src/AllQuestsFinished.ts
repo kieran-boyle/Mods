85159424f4b70351f6de442194dc09947da0f721
kieran-boyle <kieran.boyle1@outlook.com>
2022-10-10 14:16:21 +0100

    AQF updated to .ts and AKI 3.2.4.

---
+++ Toasty-AllQuestsFinished-Kiki_Repack/src/AllQuestsFinished.ts
@@ -0,0 +1,36 @@
+import { DependencyContainer } from "tsyringe"
+import type { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod"
+import type { DatabaseServer } from "@spt-aki/servers/DatabaseServer"
+
+class AllQuestsFinished implements IPostDBLoadMod
+{
+  private container: DependencyContainer
+
+  public postDBLoad(container: DependencyContainer):void
+  {
+
+    this.container = container  
+    const quests = this.container.resolve<DatabaseServer>("DatabaseServer").getTables().templates.quests
+    //The following is ripped straight from Ereshkigal's All Quests Available mod with minor changes
+
+    for (let file in quests)
+    {
+      let fileData = quests[file]
+
+      fileData.conditions.AvailableForFinish = [
+      {
+        "_parent": "Level",
+        "_props":
+        {
+          "compareMethod": ">=",
+          "value": "1",
+          "index": 0,
+          "parentId": "",
+          "id": "Exist"
+        }
+      }]
+    }
+  }
+}
+
+module.exports = {mod: new AllQuestsFinished()}
