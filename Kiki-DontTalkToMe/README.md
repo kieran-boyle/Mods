3edc9c20e90f53fa74639065719fcd5e4f2a0b29
kieran-boyle <kieran.boyle1@outlook.com>
2022-11-21 12:55:19 +0000

    Updated DTTM to add a config.

--- Kiki-DontTalkToMe/README.md
+++ Kiki-DontTalkToMe/README.md
@@ -1,1 +1,6 @@
 Makes all bots unable to talk, warning this adds considerable difficulty.
+
+*Config Explanation*
+
+muteAll:  if true will mute all bots, if false then the individual settings in botTypes are used instead.
+Then for each bot in botTypes, if muteAll is false, set to true to mute this type of bot.
--- Kiki-DontTalkToMe/src/dontTalkToMe.ts
+++ Kiki-DontTalkToMe/src/dontTalkToMe.ts
@@ -5,19 +5,36 @@ import type { DatabaseServer } from "@spt-aki/servers/DatabaseServer"
 class dontTalkToMe implements IPostDBLoadMod
 {
   private container: DependencyContainer
+  private config = require("../config/config.json")
 
   public postDBLoad(container: DependencyContainer):void
   {
     this.container = container
     const bots = this.container.resolve<DatabaseServer>('DatabaseServer').getTables().bots.types
-
+    
     for(let i in bots)
     {
-      for(let j in bots[i].difficulty)
+      if(this.config.muteAll === true)
       {
-        let bot = bots[i].difficulty[j]
-        bot.Mind.CAN_TALK = false
+        this.shutUp(bots[i])
       }
+      else
+      {
+        if((i === "assault" || i === "marksman") && this.config.botTypes.scav === true) this.shutUp(bots[i])
+        if((i === "bear" || i === "usec" )&& this.config.botTypes.pmc === true) this.shutUp(bots[i])
+        if(i === "cursedassault" && this.config.botTypes.raider === true) this.shutUp(bots[i])
+        if(i === "exusec" && this.config.botTypes.rogue === true) this.shutUp(bots[i])
+        if((i.includes("boss") || i === "sectantpriest" )&& this.config.botTypes.boss === true) this.shutUp(bots[i])
+        if((i.includes("follower") || i === "sectantwarrior" ) && this.config.botTypes.follower === true) this.shutUp(bots[i])
+      }      
+    }
+  }
+
+  private shutUp(bot :any):void
+  {
+    for(let i in bot.difficulty)
+    {
+      bot.difficulty[i].Mind.CAN_TALK = false
     }
   }
 }
