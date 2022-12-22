b17f844819d0c000190d4001fae5f32735dc9cbc
kieran-boyle <kieran.boyle1@outlook.com>
2022-12-22 02:57:16 +0000

    Used getRandomInt function in ShuffleArray

--- Kiki-AllTheBoss/src/AllTheBoss.ts
+++ Kiki-AllTheBoss/src/AllTheBoss.ts
@@ -498,7 +498,7 @@ class AllTheBoss implements IPostDBLoadMod, IPreAkiLoadMod
   {
     for (var i = array.length - 1; i > 0; i--) 
     {
-      var j = Math.floor(Math.random() * (i + 1))
+      var j = this.getRandomInt(0, i + 1)
       var temp = array[i]
       array[i] = array[j]
       array[j] = temp
