25c6fc9c4efb5318f6885872b82d7a05b76251f3
kieran-boyle <kieran.boyle1@outlook.com>
2023-01-27 00:04:50 +0000

    New mod MFIR

---
+++ Kiki-MarkFIR/README.txt
@@ -0,0 +1,3 @@
+Sets all found items to being found in raid, regardless of raid outcome.
+If the config option markEVERYTHING is set to true, it will mark all items on your charector as FIR regardless if brought into raid, 
+and regardless of raid outcome.  (true / false default = false)
---
+++ Kiki-MarkFIR/src/MarkFIR.ts
@@ -0,0 +1,58 @@
+import type { DependencyContainer } from "tsyringe"
+import type { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod"
+import { InraidController } from "@spt-aki/controllers/InraidController"
+import { InRaidHelper } from "@spt-aki/helpers/InRaidHelper"
+
+class MarkFIR implements IPreAkiLoadMod
+{
+  private container: DependencyContainer
+  private config = require("../config/config.json")
+
+  public preAkiLoad(container: DependencyContainer): void
+  {
+    this.container = container
+
+    container.afterResolution("InraidController", (_t, result: InraidController) => 
+    {
+      result.markOrRemoveFoundInRaidItems = (offraidData: ISaveProgressRequestData, pmcData: IPmcData, isPlayerScav: boolean): void => 
+      {
+        return this.InRaidControllerReplacement(offraidData, pmcData, isPlayerScav)
+      }
+    }, {frequency: "Always"})
+    
+    if(this.config.markEVERYTHING === true){
+      container.afterResolution("InRaidHelper", (_t, result: InRaidHelper) => 
+      {
+        result.addSpawnedInSessionPropertyToItems = (preRaidProfile: IPmcData, postRaidProfile: IPmcData, isPlayerScav: boolean): IPmcData => 
+        {
+          return this.InRaidHelperReplacement(preRaidProfile, postRaidProfile, isPlayerScav)
+        }
+      }, {frequency: "Always"})
+    }
+  }
+
+  private InRaidControllerReplacement(offraidData: ISaveProgressRequestData, pmcData: IPmcData, isPlayerScav: boolean): void
+  {
+    const InRaidHelper = this.container.resolve<InRaidHelper>("InRaidHelper")
+    offraidData.profile = InRaidHelper.addSpawnedInSessionPropertyToItems(pmcData, offraidData.profile, isPlayerScav)
+  }
+
+  private InRaidHelperReplacement(preRaidProfile: IPmcData, postRaidProfile: IPmcData, isPlayerScav: boolean): IPmcData
+  {
+    for (const item of postRaidProfile.Inventory.items)
+    {
+      if ("upd" in item)
+      {
+        item.upd.SpawnedInSession = true
+      }
+      else
+      {
+        item.upd = { SpawnedInSession: true }
+      }
+    }
+
+    return postRaidProfile;
+  }
+}
+
+module.exports = {mod: new MarkFIR()}
