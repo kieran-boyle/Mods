b1f3819ee7381bf4245205fb5f28d0ef8ab964e2
kieran-boyle <kieran.boyle1@outlook.com>
2025-06-17 02:13:27 +0100

    Updated smallTweaks

--- Kiki-SmallTweaks/README.md
+++ Kiki-SmallTweaks/README.md
@@ -1,5 +1,15 @@
-Adds option to change loose loot amount.
-Sets raid time to 2 hours.
-Sets the 5 second raid start timer to 1 second.
-Opens all extracts available, and removes conditions (ie price, scav co-operation).
-Sets background colour for ammo, depending on it's penetration stat.
+removeInraidRestriction = removes inraid restrictions ie amount of money etc.
+fastDeploy = removes the 5 second deploy timer.
+fleaLevel = set your flea access level here.
+openExtracts = opens all extracts.
+colourAmmo = colours ammo based on its penetration statistic.
+removeContainerRestrictions = removes inraid 
+backpack and container restrictions.
+giveKeysUnlimitedUses = gives keys unlimited uses.
+raidLength =  length of raid in minutes.
+setStrength = Sets the PMC's strength experience to be the same as it's endurance at raid end.  (if you are running something that makes things weightless, it is impossible to level strength)
+
+loot config =
+  globalsMul global loot multiplier
+  then loot multiplier in each map.
+  1 is standard, 0.5 half 2 double.
--- Kiki-SmallTweaks/src/SmallTweaks.ts
+++ Kiki-SmallTweaks/src/SmallTweaks.ts
@@ -1,10 +1,14 @@
-import type { DependencyContainer } from 'tsyringe'
-import type { IPostDBLoadMod } from '@spt/models/external/IPostDBLoadMod'
-import type { DatabaseServer } from '@spt/servers/DatabaseServer'
+import { DependencyContainer } from 'tsyringe'
+import { IPostDBLoadMod } from '@spt/models/external/IPostDBLoadMod'
+import { DatabaseServer } from '@spt/servers/DatabaseServer'
+import { ProfileHelper } from "@spt/helpers/ProfileHelper"
+import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService"
+import Config from "../config/config.json"
 
 class smallTweaks implements IPostDBLoadMod
 {
   private container: DependencyContainer
+  private config = Config
   
   public postDBLoad(container: DependencyContainer):void
   {
@@ -12,43 +16,46 @@ class smallTweaks implements IPostDBLoadMod
     this.container = container
     const database = this.container.resolve<DatabaseServer>('DatabaseServer').getTables().templates.items
     const globals = this.container.resolve<DatabaseServer>('DatabaseServer').getTables().globals
-    const locations = this.container.resolve<DatabaseServer>('DatabaseServer').getTables().locations
+    const locations = this.container.resolve<DatabaseServer>('DatabaseServer').getTables().locations    
+    const items = this.container.resolve<DatabaseServer>("DatabaseServer").getTables().templates.items
 
-    const lootConfig = //from AIO but mini
+    const keyParents = ['5c99f98d86f7745c314214b3', '5c164d2286f774194c5e69fa']
+    const containers = ["5448e53e4bdc2d60728b4567", "5448bf274bdc2dfc2f8b456a"]
+
+
+
+    //remove inraid restriction.
+    if(this.config.removeInraidRestriction === true)
+      globals.config.RestrictionsInRaid = []
+
+    //set the 5 second deploy counter to be instant.
+    if(this.config.fastDeploy === true)
     {
-      'globalsMul': 1,
-      'bigmap': 1,
-      'factory4_day': 1,
-      'factory4_night': 1,
-      'interchange': 1,
-      'laboratory': 1,
-      'shoreline': 1,
-      'woods': 1,
-      'rezervbase': 1,
-      'lighthouse': 1,
-      'tarkovstreets': 1,
+      globals.config.TimeBeforeDeploy = 1
+      globals.config.TimeBeforeDeployLocal = 1
     }
 
-    //set the 5 second deploy counter to be instant.  Raise flea level to 99
-    globals.config.TimeBeforeDeploy = 1
-    globals.config.TimeBeforeDeployLocal = 1
-    globals.config.RagFair.minUserLevel = 99
+    //Raise flea level to 99.
+    globals.config.RagFair.minUserLevel = this.config.fleaLevel
     
     //Open extracts.
-    for (let i in locations)
+    if(this.config.openExtracts === true)
     {
-      if (i !== 'base') 
+      for (let i in locations)
       {
-        this.setLocations(locations[i], this.findEnterences(locations[i]))        
+        if (i !== 'base') 
+        {
+          this.setLocations(locations[i], this.findEntrances(locations[i]))        
+        }
       }
     }
-
-    //set baground colour of ammo depending on pen
+    
     for (let i in database)
     {
       let item = database[i]
       
-      if (item._parent === '5485a8684bdc2da71d8b4567')
+      //set baground colour of ammo depending on pen
+      if (item._parent === '5485a8684bdc2da71d8b4567' && this.config.colourAmmo === true)
       {
         let pen = item._props.PenetrationPower
         let colour = ''
@@ -61,22 +68,70 @@ class smallTweaks implements IPostDBLoadMod
                 colour = 'grey' //LowPen 
         item._props.BackgroundColor = colour
       }
+
+      //remove container restrictions
+      if(containers.includes(item._parent) && this.config.removeContainerRestrictions === true)
+      {
+        item._props.Grids[0]._props.filters[0].ExcludedFilter = []
+      }
+
+      //give keys unlimited uses
+      if(keyParents.includes(item._parent) && this.config.giveKeysUnlimitedUses === true)
+      {
+        item._props.MaximumNumberOfUsage = 0
+      }
     }
 
    //Changing maps loots spawn chances multiplier and set raid time to 2 hours
-    for (let [k, v] of Object.entries(lootConfig))
+    for (let [k, v] of Object.entries(this.config.lootConfig))
     {
 
       if (k === 'globalsMul')
       {
-        globals.config.GlobalLootChanceModifier = v 
+        globals.config.GlobalLootChanceModifier = this.config.lootConfig.globalsMul
       }
       else
       {
-        locations[k].base.GlobalLootChanceModifier = v
-        locations[k].base.EscapeTimeLimit = 120
+        locations[k].base.GlobalLootChanceModifier = this.config.lootConfig[k]
+        locations[k].base.EscapeTimeLimit = this.config.raidLength
       }
     }
+  }
+
+  public preSptLoad(container: DependencyContainer):void
+  {
+    this.container = container
+    const staticRouterModService = this.container.resolve<StaticRouterModService>("StaticRouterModService")
+
+    if(this.config.setStrength === true)
+    {
+      staticRouterModService.registerStaticRouter(
+        "SetStrToEnd",
+        [{
+          url: "/client/match/offline/end",
+          action: (url :string, info :any, sessionId :string, output :string) => 
+          {
+            const profileHelper = this.container.resolve<ProfileHelper>("ProfileHelper")
+
+            this.setStrLevel(profileHelper.getPmcProfile(sessionId))
+            return output
+          }
+        }], "spt"
+      )
+    }
+  }
+
+  //sets strengths skill stats to equal that of endurance at raid end
+  private setStrLevel(profile):void
+  {
+    let skills = profile.Skills.Common
+
+    const strength = skills.find(obj => obj.Id === "Strength")
+    const endurance = skills.find(obj => obj.Id === "Endurance")
+
+    strength.Progress = endurance.Progress
+    strength.PointsEarnedDuringSession = endurance.PointsEarnedDuringSession
+    strength.LastAccess = endurance.LastAccess
   }
 
   /**
@@ -84,7 +139,7 @@ class smallTweaks implements IPostDBLoadMod
    * @param loc database locations
    * @returns comma seperated string of all entery points in map
    */
-  findEnterences(loc :any):string
+  findEntrances(loc :any):string
   {
     var enterences :string[] = []
 
